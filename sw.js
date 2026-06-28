05da96a7ade09ed9a1f99342d9ccc73ad4f70711
MakayBrooks <makaybrooks99@gmail.com>
2026-06-28 04:00:15 -0400

    SW v13 fixed: nuke caches + force reload (no infinite loop)

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('activate',e=>{
   e.waitUntil(
     caches.keys()
       .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
-      .then(()=>self.registration.unregister())
+      .then(()=>self.clients.claim())
       .then(()=>self.clients.matchAll({type:'window'}))
       .then(clients=>{clients.forEach(c=>c.navigate(c.url));})
   );
