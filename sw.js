682ac8fd66fb6b6e2906c4742171af4c348ab33c
MakayBrooks <makaybrooks99@gmail.com>
2026-06-28 03:50:59 -0400

    SW v13: self-unregister + force page reload

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-const CACHE='grindspace-v12';
+const CACHE='grindspace-v13';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+      .then(()=>self.registration.unregister())
+      .then(()=>self.clients.matchAll({type:'window'}))
+      .then(clients=>{clients.forEach(c=>c.navigate(c.url));})
+  );
 });
 self.addEventListener('fetch',e=>{
   e.respondWith(fetch(e.request.clone()).catch(()=>caches.match(e.request)));
